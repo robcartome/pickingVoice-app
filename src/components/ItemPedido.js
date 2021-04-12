a9f721c4cc1a3806515571d1db00407631e2b772
Robert <robert.tm2018@gmail.com>
2021-04-11 23:02:45 -0500

    Added function active Voice for speech ubicacion and more

--- src/components/ItemPedido.js
+++ src/components/ItemPedido.js
@@ -1,13 +1,13 @@
 import styled from "@emotion/styled";
-import {  useState } from "react";
-import iconVoice from '../assets/voice.svg';
+import { useState } from "react";
+import iconVoice from "../assets/voice.svg";
 
 export default function ItemPedido(props) {
-  const [isOpened, setIsOpened] = useState(false)
+  const [isOpened, setIsOpened] = useState(false);
   const showModal = () => {
     setIsOpened(!isOpened);
-    console.log(isOpened)
-  }
+    console.log(isOpened);
+  };
 
   const showButton =
     props.type == "header" ? (
@@ -23,25 +23,64 @@ export default function ItemPedido(props) {
         <div>
           <p>{props.estado}</p>
         </div>
-        <button onClick={(e)=>showModal()}>show</button>
+        <button onClick={(e) => showModal()}>show</button>
       </>
     );
+  /*  */
+
+  function activeVoice(type, data) {
+    let speech;
+    let msgIntro;
+
+    switch(type){
+      case "ubicar":
+        msgIntro = "Ubicar: ";
+      break;
+      case "cantidad":
+        msgIntro = "Cantidad: ";
+      break;
+      case "completo":
+        msgIntro = "Completado Siguiente";
+      break;
+    }
+
+    speech = new SpeechSynthesisUtterance(msgIntro);
+    speech.lang = "es-ES";
+    speech.rate = 0.9;
+    window.speechSynthesis.speak(speech);
+    for (let indice = 0; indice < data.length; indice++)
+      for (let [key, value] of Object.entries(data[indice])) {
+        speech = new SpeechSynthesisUtterance(key);
+        speech.lang = "es-ES";
+        speech.volume = 1;
+        window.speechSynthesis.speak(speech);
+        speech = new SpeechSynthesisUtterance(value);
+        speech.lang = "es-ES";
+        window.speechSynthesis.speak(speech);
+      }
+  }
+
+  /*  */
   return (
     <ProductoItem>
       {props.children}
       {showButton}
-      <Modal state={isOpened?"flex":"none"} >
+      <Modal state={isOpened ? "flex" : "none"}>
         <ModalWrapper>
           <div>
             <h4>Picking Voice</h4>
             <h3>PDO: 163268 (2/5)</h3>
           </div>
-          <ButtonVoice ><img src={iconVoice}/></ButtonVoice>
+          <ButtonVoice
+            onClick={(e) => activeVoice("completo",[{ rack: 1, "": "B", nivel: 3 }])}
+          >
+            <img src={iconVoice} />
+          </ButtonVoice>
           <p>Ubicacion Almacen</p>
-          <MapWrapper>
-                Mapa
-          </MapWrapper>
-          <button className="button--cancel" onClick={(e)=>showModal()}>CANCEL</button>
+          <MapWrapper>Mapa</MapWrapper>
+          <button className="button--cancel" onClick={(e) => showModal()}>
+            CANCEL
+          </button>
         </ModalWrapper>
       </Modal>
     </ProductoItem>
@@ -73,25 +112,24 @@ const ProductoItem = styled.li`
 `;
 
 const Modal = styled.div`
-  background-color: rgba(0,0,0,0.2);
+  background-color: rgba(0, 0, 0, 0.2);
   position: fixed;
   top: 0;
   left: 0;
   height: 100vh;
   width: 100vw;
-  transition: all .3s;
-  display:${(props)=>props.state} ;
+  transition: all 0.3s;
+  display: ${(props) => props.state};
   align-items: center;
   justify-content: center;
-  
 `;
 const ModalWrapper = styled.div`
-  background: #FFFFFF;
+  background: #ffffff;
   box-shadow: 0px 2px 20px rgba(0, 0, 0, 0.25);
   border-radius: 16px 16px 0px 0px;
   width: 100%;
   padding: 10px;
-  margin:10px;
+  margin: 10px;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -99,11 +137,12 @@ const ModalWrapper = styled.div`
   & > div {
     text-align: center;
   }
-  & p,h4 {
+  & p,
+  h4 {
     color: green;
   }
   & .button--cancel {
-    background: #C4C4C4;
+    background: #c4c4c4;
     color: #ffffff;
     font-size: 16px;
     border: none;
@@ -115,26 +154,26 @@ const ModalWrapper = styled.div`
 
 const ButtonVoice = styled.button`
   background: rgba(75, 148, 96, 0.1);
-  width:60px;
-  height:60px;
+  width: 60px;
+  height: 60px;
   margin: 5px;
-  padding-right:8px;
-  border: 2px solid #4B9460;
+  padding-right: 8px;
+  border: 2px solid #4b9460;
   box-sizing: border-box;
   border-radius: 50%;
   display: flex;
   align-items: center;
   justify-content: center;
   cursor: pointer;
-  outline:none;
+  outline: none;
   :active {
-    background-color:rgb(153,255,204,0.9);
+    background-color: rgb(153, 255, 204, 0.9);
   }
 `;
 
 const MapWrapper = styled.div`
   background: #f6f6f9;
   width: 100%;
-  max-width:485px;
+  max-width: 485px;
   height: 192px;
 `;
