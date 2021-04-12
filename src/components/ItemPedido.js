42cf92debd01e93981a88f14260b471dbfb5eac1
Robert <robert.tm2018@gmail.com>
2021-04-11 21:22:46 -0500

    Added show pedido wit modal

--- src/components/ItemPedido.js
+++ src/components/ItemPedido.js
@@ -1,6 +1,14 @@
 import styled from "@emotion/styled";
+import {  useState } from "react";
+import iconVoice from '../assets/voice.svg';
 
 export default function ItemPedido(props) {
+  const [isOpened, setIsOpened] = useState(false)
+  const showModal = () => {
+    setIsOpened(!isOpened);
+    console.log(isOpened)
+  }
+
   const showButton =
     props.type == "header" ? (
       <div></div>
@@ -15,13 +23,27 @@ export default function ItemPedido(props) {
         <div>
           <p>{props.estado}</p>
         </div>
-        <button>show</button>
+        <button onClick={(e)=>showModal()}>show</button>
       </>
     );
   return (
     <ProductoItem>
       {props.children}
       {showButton}
+      <Modal state={isOpened?"flex":"none"} >
+        <ModalWrapper>
+          <div>
+            <h4>Picking Voice</h4>
+            <h3>PDO: 163268 (2/5)</h3>
+          </div>
+          <ButtonVoice ><img src={iconVoice}/></ButtonVoice>
+          <p>Ubicacion Almacen</p>
+          <MapWrapper>
+                Mapa
+          </MapWrapper>
+          <button className="button--cancel" onClick={(e)=>showModal()}>CANCEL</button>
+        </ModalWrapper>
+      </Modal>
     </ProductoItem>
   );
 }
@@ -38,7 +60,7 @@ const ProductoItem = styled.li`
   min-width: 315px;
   width: 60%;
   border: 1px solid #f6f6f9;
-  button {
+  & > button {
     background: #5db075;
     color: #ffffff;
     font-size: 16px;
@@ -48,4 +70,71 @@ const ProductoItem = styled.li`
     outline: none;
     cursor: pointer;
   }
+`;
+
+const Modal = styled.div`
+  background-color: rgba(0,0,0,0.2);
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100vw;
+  transition: all .3s;
+  display:${(props)=>props.state} ;
+  align-items: center;
+  justify-content: center;
+  
+`;
+const ModalWrapper = styled.div`
+  background: #FFFFFF;
+  box-shadow: 0px 2px 20px rgba(0, 0, 0, 0.25);
+  border-radius: 16px 16px 0px 0px;
+  width: 100%;
+  padding: 10px;
+  margin:10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  & > div {
+    text-align: center;
+  }
+  & p,h4 {
+    color: green;
+  }
+  & .button--cancel {
+    background: #C4C4C4;
+    color: #ffffff;
+    font-size: 16px;
+    border: none;
+    border-radius: 30px;
+    padding: 8px 10px;
+    cursor: pointer;
+  }
+`;
+
+const ButtonVoice = styled.button`
+  background: rgba(75, 148, 96, 0.1);
+  width:60px;
+  height:60px;
+  margin: 5px;
+  padding-right:8px;
+  border: 2px solid #4B9460;
+  box-sizing: border-box;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  outline:none;
+  :active {
+    background-color:rgb(153,255,204,0.9);
+  }
+`;
+
+const MapWrapper = styled.div`
+  background: #f6f6f9;
+  width: 100%;
+  max-width:485px;
+  height: 192px;
 `;
--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -1,27 +1,67 @@
 import ItemPedido from "../components/ItemPedido";
 import styled from "@emotion/styled";
+import { useEffect, useState } from "react";
 
 export default function Orders() {
+  const [statePedido, setStatePedido] = useState(0);
 
-  return (<div>
-    <h1>Pedidos</h1>
-    <List>
-      <ItemPedido type={"header"}>
-        <div><h5>#CodPdo</h5></div><div><h5>Cliente</h5></div><div><h5>Estado</h5></div>
-      </ItemPedido>
-      <ItemPedido id = {"1"} cod ={"136268"} cliente={"Wilfredo sala ruperto"} estado={"False"}>
-      </ItemPedido>
-    </List>
-
-  </div>)
+  const data = [
+    {
+      id: 1,
+      codPdo: "136268",
+      cliente: "Wilfredo sala ruperto",
+      estado: 0,
+    },
+    {
+      id: 2,
+      codPdo: "136234",
+      cliente: "Robert Tolemtnoio",
+      estado: 0,
+    },
+    {
+      id: 3,
+      codPdo: "136244",
+      cliente: "Kathy Jara Larcon",
+      estado: 0,
+    },
+  ];
+  return (
+    <div>
+      <h1>Pedidos</h1>
+      <List>
+        <ItemPedido type={"header"}>
+          <div>
+            <h5>#CodPdo</h5>
+          </div>
+          <div>
+            <h5>Cliente</h5>
+          </div>
+          <div>
+            <h5>Estado</h5>
+          </div>
+        </ItemPedido>
+        {data.map((pedido) => {
+          return (
+            <ItemPedido
+              key={pedido.id}
+              id={pedido.id}
+              cod={pedido.codPdo}
+              cliente={pedido.cliente}
+              estado={pedido.estado}
+            />
+          );
+        })}
+      </List>
+    </div>
+  );
 }
 
 const List = styled.ul`
-margin-right: 10%;
-list-style-type: none;
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-gap: 4px;
+  margin-right: 10%;
+  list-style-type: none;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 4px;
 `;
